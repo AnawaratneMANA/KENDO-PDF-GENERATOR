d35f31508cf621a339b6d3c5d61f9bd63434e1f3
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-30 10:41:48 +0530

    Main styles added to the App.css

--- src/App.js
+++ src/App.js
@@ -4,21 +4,9 @@ import '@progress/kendo-theme-material/dist/all.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1> Kendo React PDF Generator.</h1>
+        <p> PDF generating tools for react.</p>
     </div>
   );
 }
