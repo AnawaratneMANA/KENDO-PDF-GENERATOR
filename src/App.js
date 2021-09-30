ee91a1449954523f7d8fa5e957e61ab653004d3b
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-30 10:52:22 +0530

    PDF exporting working with Kendo PDF generator.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import {useRef} from 'react'
 import './App.css';
 import '@progress/kendo-theme-material/dist/all.css';
 import { Button } from '@progress/kendo-react-buttons';
+import {PDFExport, savePDF} from "@progress/kendo-react-pdf";
 
 function App() {
 
+  const pdfExportComponent = useRef(null);
 
   const handleExportWithComponent = (event) => {
-    console.log("Clicked")
+    console.log("Clicked");
+    pdfExportComponent.current.save();
   }
 
   return (
     <div className="app-content">
+      <PDFExport ref={pdfExportComponent} paperSize="A4">
         <h1> Kendo React PDF Generator.</h1>
         <p> PDF generating tools for react.</p>
         <div className="button-area">
           <Button primary={true} onClick={handleExportWithComponent}>Primary Button</Button>
         </div>
+        </PDFExport>
     </div>
   );
 }
