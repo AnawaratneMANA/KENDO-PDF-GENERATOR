fc21dd682f21186d99935527214fdf4d96672fcc
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-30 10:46:58 +0530

    onClick Listener created.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import '@progress/kendo-theme-material/dist/all.css'
+import '@progress/kendo-theme-material/dist/all.css';
+import { Button } from '@progress/kendo-react-buttons';
 
 function App() {
+
+
+  const handleExportWithComponent = (event) => {
+    console.log("Clicked")
+  }
+
   return (
-    <div>
+    <div className="app-content">
         <h1> Kendo React PDF Generator.</h1>
         <p> PDF generating tools for react.</p>
+        <div className="button-area">
+          <Button primary={true} onClick={handleExportWithComponent}>Primary Button</Button>
+        </div>
     </div>
   );
 }
